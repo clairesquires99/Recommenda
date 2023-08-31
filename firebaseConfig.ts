91549855f7257376e3d5164f213b49b3204d62db
Claire Squires <claires@theodo.co.uk>
2023-08-31 11:05:41 +0100

    chore: initial upload

---
+++ firebaseConfig.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyATcAvuhNqk-IcV6kRCxT0z1guWY5kCrd0",
+  authDomain: "recommenda-886b6.firebaseapp.com",
+  projectId: "recommenda-886b6",
+  storageBucket: "recommenda-886b6.appspot.com",
+  messagingSenderId: "499990112696",
+  appId: "1:499990112696:web:2ff211a6a613949dc5474d",
+};
+
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIRESTORE_DB = getFirestore(FIREBASE_APP);
