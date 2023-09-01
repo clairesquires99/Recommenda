216fd158ed13e670e04c20606bd1c2f602fa9783
Claire Squires <claires@theodo.co.uk>
2023-09-01 17:01:34 +0100

    feat: basic search

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo", "module:metro-react-native-babel-preset"],
+    presets: ["babel-preset-expo"],
     plugins: [
       // Required for expo-router
       "expo-router/babel",
-      "module:react-native-dotenv",
-      {
-        envName: "APP_ENV",
-        moduleName: "@env",
-        path: ".env",
-        safe: false,
-        allowUndefined: true,
-        verbose: false,
-      },
     ],
   };
 };
--- src/features/books/BookSearchScreen.tsx
+++ src/features/books/BookSearchScreen.tsx
@@ -1,13 +1,27 @@
-import { Button, FlatList, SafeAreaView, Text, TextInput } from "react-native";
+import {
+  Button,
+  FlatList,
+  Pressable,
+  SafeAreaView,
+  Text,
+  TextInput,
+  View,
+  Image,
+} from "react-native";
 import { styles } from "../../styles";
 import { useState } from "react";
 import axios from "axios";
 import { GOOGLE_BOOKS_API_KEY } from "../../../env";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 type Book = {
   id: string;
   volumeInfo: {
     title: string;
+    authors: string[];
+    imageLinks: {
+      thumbnail: string;
+    };
   };
 };
 
@@ -27,18 +41,38 @@ export const BookSearchScreen = () => {
     }
   };
 
+  const renderItem = (item: Book) => (
+    <View style={styles.card}>
+      <Image
+        source={{ uri: item.volumeInfo.imageLinks.thumbnail }}
+        style={styles.cardImage}
+      />
+      <View style={styles.cardText}>
+        <Text style={styles.bookTitle}>{item.volumeInfo.title}</Text>
+        <Text style={styles.bookAuthor}>By {item.volumeInfo.authors}</Text>
+      </View>
+    </View>
+  );
+
   return (
     <SafeAreaView style={styles.container}>
-      <TextInput
-        placeholder="Enter book title or author"
-        value={query}
-        onChangeText={setQuery}
-      />
-      <Button title="Search" onPress={searchBooks} />
+      <View style={styles.inputSearchContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter book title or author..."
+          value={query}
+          onChangeText={setQuery}
+          placeholderTextColor="#aaa"
+        />
+        <Pressable style={styles.searchButton} onPress={searchBooks}>
+          <FontAwesome name="search" style={{ fontSize: 20 }} />
+        </Pressable>
+      </View>
       <FlatList
         data={books}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <Text>{item.volumeInfo.title}</Text>}
+        renderItem={(item) => renderItem(item.item)}
+        style={styles.cardContainer}
       />
     </SafeAreaView>
   );
