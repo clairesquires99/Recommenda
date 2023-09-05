770dd654bb6c1c98416b793625d97b21a7f8d7eb
Claire Squires <claires@theodo.co.uk>
2023-09-05 15:18:23 +0100

    feat: add firebase auth to config

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyATcAvuhNqk-IcV6kRCxT0z1guWY5kCrd0",
@@ -12,3 +13,4 @@ const firebaseConfig = {
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIRESTORE_DB = getFirestore(FIREBASE_APP);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
