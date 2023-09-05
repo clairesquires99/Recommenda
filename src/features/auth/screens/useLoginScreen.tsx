91be91a5dfc974814d577200ce0eed414214059a
Claire Squires <claires@theodo.co.uk>
2023-09-05 15:19:29 +0100

    feat: login screen and functionality

---
+++ src/features/auth/screens/useLoginScreen.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { FIREBASE_AUTH } from "../../../../firebaseConfig";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from "expo-router";
+
+export const useLoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleLogin = () => {
+    signInWithEmailAndPassword(FIREBASE_AUTH, email, password)
+      .then((userCredentials) => {
+        const user = userCredentials.user;
+        router.push("/");
+      })
+      .catch((error) => alert(error.message));
+  };
+
+  return {
+    email,
+    setEmail,
+    password,
+    setPassword,
+    handleLogin,
+  };
+};
