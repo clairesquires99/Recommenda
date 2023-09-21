7bb259f9c33f3b87320675959d2b9791bc779c19
Claire Squires <claires@theodo.co.uk>
2023-09-21 10:03:04 +0100

    feat: createNewFollow method

--- src/utils/addItem.tsx
+++ src/utils/addItem.tsx
@@ -1,5 +1,6 @@
 import { addDoc, collection } from "firebase/firestore";
 import { FIRESTORE_DB } from "../../firebaseConfig";
+import { User } from "firebase/auth";
 
 export const addItem = async () => {
   const doc = await addDoc(collection(FIRESTORE_DB, "test_items"), {
@@ -7,4 +8,22 @@ export const addItem = async () => {
     author: "rob",
   });
   console.log("Object added to DB with ID: ", doc.id);
+};
+
+interface createNewFollowProps {
+  user: User;
+  following: User;
+}
+
+export const createNewFollow = async ({
+  user,
+  following,
+}: createNewFollowProps) => {
+  const doc = await addDoc(collection(FIRESTORE_DB, "follows"), {
+    user: user.uid,
+    userName: user.displayName,
+    following: following.uid,
+    followingName: user.displayName,
+  });
+  console.log(user, "is now following", following);
 };
