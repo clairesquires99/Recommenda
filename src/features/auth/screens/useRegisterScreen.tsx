edfd890aa0ddf33908236e3c461080d0c95fb094
Claire Squires <claires@theodo.co.uk>
2023-09-26 17:31:45 +0100

    fix: corrected formatting for fullName

--- src/features/auth/screens/useRegisterScreen.tsx
+++ src/features/auth/screens/useRegisterScreen.tsx
@@ -7,7 +7,7 @@ import { useAuthStore } from "../../../utils/store";
 export const useRegisterScreen = () => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
-  const fullName = firstName.concat(lastName);
+  const fullName = firstName + " " + lastName;
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
